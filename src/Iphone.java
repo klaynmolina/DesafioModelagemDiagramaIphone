827579fce9be043af8595ed6c04a2b43ade752b5
Molina <83372957+klaynmolina@users.noreply.github.com>
2023-08-31 18:32:00 -0300

    criação da classe iphone

---
+++ src/Iphone.java
@@ -0,0 +1,57 @@
+public class Iphone implements MusicPlayer, MobileDevice, InternetBrowser{
+
+	@Override
+	public void displayPage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addNewTab() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void refreshPage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void call() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void answer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void voiceMail() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void selectMusic() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
